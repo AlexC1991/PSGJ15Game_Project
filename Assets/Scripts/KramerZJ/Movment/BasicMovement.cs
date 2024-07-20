95d4a66b66bbc9ec5a5c9ffb89d0d9b1dd8dc144
KramerZJ <kramerzj@gmail.com>
2024-07-20 12:38:00 -0400

    Transform Based Movement
    It's transform based movements including jump and gravity

--- Assets/Scripts/KramerZJ/Movment/BasicMovement.cs
+++ Assets/Scripts/KramerZJ/Movment/BasicMovement.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Collections;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -13,20 +14,27 @@ namespace ProjectDungeonCrawlerPJ15
         [Header("Movement")]
         [SerializeField, Range(0, 50f)] private float _speed = 7f;
         [SerializeField, Range(-20f, 0)] private float _gravity = -9.8f;
-        [SerializeField, Range(0.4f, 1f)] private float _checkLength = 0.1f;
-        [SerializeField, Range(0, 20f)] private float _jumpForce = 12f;
+        [SerializeField, Range(0.1f, 1f)] private float _checkLength = 0.1f;
+        private Vector3 _horizontalVelocity = new Vector3(0f, 0f, 0f);
+        private Vector3 _verticalVelocity = new Vector3(0f, 0f, 0f);
+
+        [Header("Jump attributes")]
         [SerializeField, Range(0, 1f)] private float _jumpTime = 0.3f;
         [SerializeField, Range(0f, 1f)] private float _jumpCooldown = 0.3f;
-        [SerializeField, Range(0f, 10f)] private float _jumpHeight = 4f;
-        [SerializeField, Range(0, 1f)] private float _airControl = 0.4f;
+        [SerializeField, Range(0f, 10f)] private float _jumpForce = 4f;//it's actually height
+        [SerializeField, Range(0, 1f)] private float _airControl = 0.2f;
+        //[SerializeField, Range(0f, 10f)] private float _jumpDistance = 4f;
+
+        [Header("Drag for velocity")]
         [SerializeField, Range(0, 10f)] private float _groundDrag = 3f;
         [SerializeField, Range(0f, 5f)] private float _airDrag = 0.5f;
-        [SerializeField, Range(0f,20f)] private float _horsePower = 10f;
+        [SerializeField, Range(0f, 1f)] private float _hitWallDrag = 0.5f;
+
+        [Header("Collition Detect")]
+        [SerializeField, Range(0f, 5f)] private float _collitionDistance = 1.5f;
+        [SerializeField, Range(0f, 2f)] private float _moveUpStep = 0.25f;
 
   
-        private float fallingTime = 0f;
-        private Vector3 _velocity;
-
         [Header("Ground Check")]
         [SerializeField] private Transform _groundCheck;
         [SerializeField] private LayerMask _groundMask;
@@ -47,9 +55,9 @@ namespace ProjectDungeonCrawlerPJ15
         [SerializeField] private Transform _cameraFollow;
 
         [Header("Input")]
-        private float _moveHorizontal;
-        private float _moveVertical;
-        private Vector3 _moveDirection;
+        [ReadOnly][SerializeField] float _moveHorizontal;
+        [ReadOnly][SerializeField] private float _moveVertical;
+        [ReadOnly][SerializeField] private Vector3 _moveDirection;
 
         
         [Header("Keybinds")]
@@ -87,7 +95,6 @@ namespace ProjectDungeonCrawlerPJ15
             
             MyInput();
             SpeedControl();
-
             //TODO: handle drag somewhere 
         }
 
@@ -100,7 +107,7 @@ namespace ProjectDungeonCrawlerPJ15
             if (Input.GetKey(jumpKey)  && _isGround&& _readyToJump)
             {
                 _readyToJump = false;
-                StartCoroutine(Jump());
+                Jump();
                 Invoke(nameof(ResetJump), _jumpCooldown);
             }
         }
@@ -111,18 +118,9 @@ namespace ProjectDungeonCrawlerPJ15
         }
 
 
-        private IEnumerator Jump()
+        private void Jump()
         {
-            float timer = 0;
-            float jumpFactor = _jumpTime / 30;
-            while (timer< _jumpTime && jumpFactor< _jumpTime)
-            {
-                transform.position = transform.position + Vector3.up * _jumpForce* -Mathf.Log(jumpFactor/ _jumpTime,30);//gravity is a negative number
-                timer += Time.fixedDeltaTime;
-                jumpFactor+= Time.fixedDeltaTime;
-                yield return new WaitForEndOfFrame();
-            }
-            
+            _verticalVelocity = Vector3.up * 2*_jumpForce/_jumpTime;
         }
         private void HandleCamera()
         {
@@ -141,51 +139,101 @@ namespace ProjectDungeonCrawlerPJ15
         }
         private void MovePlayer()
         {
+            //update position
+            transform.position += _horizontalVelocity * Time.fixedDeltaTime + _verticalVelocity * Time.fixedDeltaTime;
+
             // calculate movement direction
             _moveDirection = transform.forward * _moveVertical + transform.right * _moveHorizontal;
-            
 
             if (OnSlope())
             {
                 //_rb.AddForce(GetSlopeMoveDirection() * _speed * _horsePower, ForceMode.Force);
 
                 //if (_rb.velocity.y > 0) _rb.AddForce(Vector3.down * 8f * _horsePower, ForceMode.Force);
-                transform.position = transform.position + GetSlopeMoveDirection() *_speed * Time.fixedDeltaTime;
+                _horizontalVelocity += GetSlopeMoveDirection() * _speed;
             }
 
             // on ground
             if (_isGround)
             //_rb.AddForce(_moveDirection.normalized * _speed * _horsePower, ForceMode.Force);
             {
-                transform.position = transform.position + _moveDirection * _speed * Time.fixedDeltaTime;
-                fallingTime = 0;
+                _horizontalVelocity += _moveDirection * _speed;
+
             }
             // in air
             else if (!_isGround)
             {
-                transform.position = transform.position + _moveDirection * _speed * _airControl * Time.fixedDeltaTime;
+                _horizontalVelocity += _moveDirection * _speed * _airControl;
                 //handle falling
-                fallingTime += Time.fixedDeltaTime;
-                if (fallingTime>0)
+                _verticalVelocity += Vector3.up * -2 * _jumpForce / (_jumpTime * _jumpTime) * Time.fixedDeltaTime;
+            }
+            CollideDetect();
+            HandleDrag();
+            //TODO: disable gravity on slope if needs
+        }
+
+        private void HandleDrag()
+        {
+            //handle drag
+            if (_isGround && _horizontalVelocity.sqrMagnitude > 0f)
+            {
+                _horizontalVelocity = _horizontalVelocity * (10 - _groundDrag) / 10;
+            }
+            else
+            {
+                _horizontalVelocity = _horizontalVelocity * (10 - _airDrag) / 10;
+            }
+            //stop when hit's ground
+            if (_isGround && _verticalVelocity.y < 0f)
+            {
+                _verticalVelocity *= 0f;
+            }
+            //stop infinite sliding
+            if (_horizontalVelocity.sqrMagnitude > 0f && _horizontalVelocity.sqrMagnitude < 0.1f)
+            {
+                _horizontalVelocity *= 0f;
+            }
+        }
+
+        private void CollideDetect()
+        {
+            RaycastHit collisionHit;
+            
+            for (int i=0;i<4;i++)
+            {
+                if (Physics.Raycast(transform.position + new Vector3(0f, _moveUpStep * 2, 0f), _horizontalVelocity.normalized, out collisionHit, _collitionDistance, _groundMask))
                 {
-                    transform.position = transform.position + Vector3.up * _gravity * Mathf.Log10(fallingTime+1);//gravity is a negative number
+                    _horizontalVelocity = Vector3.ProjectOnPlane(_horizontalVelocity, collisionHit.normal) * _hitWallDrag;
+                }
+                if (Physics.Raycast(transform.position + new Vector3(0f, _moveUpStep * 6, 0f), _horizontalVelocity.normalized, out collisionHit, _collitionDistance, _groundMask))
+                {
+                    _horizontalVelocity = Vector3.ProjectOnPlane(_horizontalVelocity, collisionHit.normal) * _hitWallDrag;
                 }
             }
-            
-
-
-            //TODO: disable gravity on slope if needs
+            if (Physics.Raycast(transform.position + new Vector3(0f, _moveUpStep * 2, 0f), _horizontalVelocity.normalized, out collisionHit, _collitionDistance, _groundMask))
+            {
+                _horizontalVelocity = Vector3.ProjectOnPlane(_horizontalVelocity, collisionHit.normal) * _hitWallDrag;
+                return;
+            }
+            if (Physics.Raycast(transform.position, _horizontalVelocity.normalized, _collitionDistance, _groundMask))
+            {
+                if (_horizontalVelocity.sqrMagnitude>0f)
+                {
+                    _verticalVelocity += new Vector3(0f, _moveUpStep, 0f);
+                }
+            }
         }
         private void SpeedControl()
         {
-            // limiting speed on slope
-            if (OnSlope())
+
+            if (_horizontalVelocity.sqrMagnitude > _speed * _speed)
             {
-                //TODO: Handle speed in transform
+                _horizontalVelocity = _horizontalVelocity.normalized * _speed;
             }
-            else// limiting speed on gournd or air
+            if (_verticalVelocity.sqrMagnitude> _speed * _speed* 2 * 2)
             {
-                //TODO: Handle speed in transform
+                _verticalVelocity = _verticalVelocity.normalized * 2*_speed;
+                Debug.Log("speed limit");
             }
         }
         private bool OnSlope()
@@ -205,6 +253,10 @@ namespace ProjectDungeonCrawlerPJ15
         {
             Gizmos.color = Color.red;
             Gizmos.DrawSphere(_groundCheck.position, _checkLength);
+            Gizmos.DrawRay(transform.position+new Vector3(0f, _moveUpStep*2, 0f), _horizontalVelocity);
+            Gizmos.DrawRay(transform.position+new Vector3(0f, _moveUpStep, 0f), _horizontalVelocity);
+            Gizmos.DrawRay(transform.position+new Vector3(0f, _moveUpStep*6, 0f), _horizontalVelocity);
+            Gizmos.DrawRay(transform.position, _horizontalVelocity);
         }
     }
 }
